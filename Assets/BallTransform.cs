b6757f02b7ae71a65f711d665ed1e5b1f811694d
atsu-tsu <fdferreira93@gmail.com>
2023-03-13 16:16:09 +0000

    3.Increment the scale

--- Assets/BallTransform.cs
+++ Assets/BallTransform.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class BallTransform : MonoBehaviour
 {
+
+    public Vector3 scaleChange;
+
+
     // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
     // Update is called once per frame
+
     void Update()
     {
-        
+        transform.localScale += scaleChange;
     }
 }
