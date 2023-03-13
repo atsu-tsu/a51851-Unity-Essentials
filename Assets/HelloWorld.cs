f80a6ed14d0cdaa758f13b4733f38621fa0e0a5e
atsu-tsu <fdferreira93@gmail.com>
2023-03-13 16:12:30 +0000

    5.Add a property with a variable

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public string myMessage;
+    
     void Start()
+
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        Debug.Log("Hello World");
+        Debug.Log(myMessage);
     }
 }
